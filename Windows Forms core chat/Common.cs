e8aa64fe3585b1b7f45c309886495108b2c87d78
TimDeveloper97 <timdeveloper97@gmail.com>
2023-11-23 20:59:59 +0700

    Add project files.

---
+++ Windows Forms core chat/Common.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Windows_Forms_CORE_CHAT_UGH
+{
+    public class Common
+    {
+        public const string C_USERNAME = "!username";
+        public const string C_USER = "!user";
+        public const string C_COMMANDS = "!commonds";
+        public const string C_WHO = "!who";
+        public const string C_ABOUT = "!about";
+        public const string C_WHISPER = "!whisper";
+        public const string C_MOD = "!mod";
+        public const string C_MODS = "!mods";
+        public const string C_KICK   = "!kick";
+        public const string SPACE   = " ";
+    }
+}
---
+++ Windows Forms core chat/TCPChatClient.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+using Windows_Forms_CORE_CHAT_UGH;
+
+//reference: https://github.com/AbleOpus/NetworkingSamples/blob/master/MultiClient/Program.cs
+namespace Windows_Forms_Chat
+{
+    public class TCPChatClient : TCPChatBase
+    {
+        public string _name = null;
+        //public static TCPChatClient tcpChatClient;
+        public Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        public ClientSocket clientSocket = new ClientSocket();
+
+
+        public int serverPort;
+        public string serverIP;
+
+
+        public static TCPChatClient CreateInstance(int port, int serverPort, string serverIP, TextBox chatTextBox)
+        {
+            TCPChatClient tcp = null;
+            //if port values are valid and ip worth attempting to join
+            if (port > 0 && port < 65535 &&
+                serverPort > 0 && serverPort < 65535 &&
+                serverIP.Length > 0 &&
+                chatTextBox != null)
+            {
+                tcp = new TCPChatClient();
+                tcp.port = port;
+                tcp.serverPort = serverPort;
+                tcp.serverIP = serverIP;
+                tcp.chatTextBox = chatTextBox;
+                tcp.clientSocket.socket = tcp.socket;
+
+            }
+
+            return tcp;
+        }
+
+        public void ConnectToServer()
+        {
+            int attempts = 0;
+
+            while (!socket.Connected)
+            {
+                try
+                {
+                    attempts++;
+                    SetChat("Connection attempt " + attempts);
+                    // Change IPAddress.Loopback to a remote IP to connect to a remote host.
+                    socket.Connect(serverIP, serverPort);
+                }
+                catch (SocketException)
+                {
+                    chatTextBox.Text = "";
+                }
+            }
+
+            //Console.Clear();
+            AddToChat("Connected");
+            //keep open thread for receiving data
+            clientSocket.socket.BeginReceive(clientSocket.buffer, 0, ClientSocket.BUFFER_SIZE, SocketFlags.None, ReceiveCallback, clientSocket);
+        }
+
+        public void SendString(string text)
+        {
+            //checked username exist
+            if (_name == null
+                && !text.ToLower().Contains(Common.C_USERNAME + Common.SPACE))
+            {
+                MessageBox.Show("You must enter name befor chat");
+                return;
+            }
+
+            //check username empty
+            var username = text.Replace(Common.C_USERNAME, "").Trim();
+            if (string.IsNullOrEmpty(username))
+            {
+                MessageBox.Show("Username can't null or empty");
+                return;
+            }
+
+            // username has set
+            //if (!string.IsNullOrEmpty(_name)
+            //    && text.ToLower().Contains(Common.C_USERNAME + Common.SPACE))
+            //{
+            //    MessageBox.Show("Command fail");
+            //    return;
+            //}
+
+            byte[] buffer = Encoding.ASCII.GetBytes(text);
+            socket.Send(buffer, 0, buffer.Length, SocketFlags.None);
+        }
+
+
+        public void ReceiveCallback(IAsyncResult AR)
+        {
+            ClientSocket currentClientSocket = (ClientSocket)AR.AsyncState;
+
+            int received;
+
+            try
+            {
+                received = currentClientSocket.socket.EndReceive(AR);
+            }
+            catch (SocketException)
+            {
+                AddToChat("Client forcefully disconnected");
+                // Don't shutdown because the socket may be disposed and its disconnected anyway.
+                currentClientSocket.socket.Close();
+                return;
+            }
+            //read bytes from packet
+            byte[] recBuf = new byte[received];
+            Array.Copy(currentClientSocket.buffer, recBuf, received);
+            //convert to string so we can work with it
+            string text = Encoding.ASCII.GetString(recBuf);
+            Console.WriteLine("Received Text: " + text);
+
+            //text is from server but could have been broadcast from the other clients
+            AddToChat(text);
+
+            #region handle add and check user name
+            if (text.ToLower().Contains(Common.C_KICK))
+            {
+                Close();
+                return;
+            }
+
+            #endregion
+
+            //we just received a message from this socket, better keep an ear out with another thread for the next one
+            currentClientSocket.socket.BeginReceive(currentClientSocket.buffer, 0, ClientSocket.BUFFER_SIZE, SocketFlags.None, ReceiveCallback, currentClientSocket);
+        }
+
+        public void Close()
+        {
+            socket.Close();
+        }
+
+        public bool IsClose() => socket.Connected;
+    }
+
+}
