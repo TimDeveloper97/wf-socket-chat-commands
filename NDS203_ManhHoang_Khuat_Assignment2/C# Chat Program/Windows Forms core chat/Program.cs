1fccc00b56234234591e10f900f506fa94920b36
DinhDuyAnh <duyanh.dinh@veriserve-vietnam.com>
2023-12-04 09:04:37 +0700

    change to .net core 3.1

--- NDS203_ManhHoang_Khuat_Assignment2/C# Chat Program/Windows Forms core chat/Program.cs
+++ NDS203_ManhHoang_Khuat_Assignment2/C# Chat Program/Windows Forms core chat/Program.cs
@@ -14,7 +14,7 @@ namespace Windows_Forms_Chat
         [STAThread]
         static void Main()
         {
-            //Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
